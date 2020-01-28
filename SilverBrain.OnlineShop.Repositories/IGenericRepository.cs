fe80d826376dcf5fdd74c905cd657a51010796e8
silverbr <sinaataei@outlook.com>
2020-01-28 15:27:28 +0330

    New layers are added to project, generic repo is under construction.

---
+++ SilverBrain.OnlineShop.Repositories/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silverbrain.OnlineShop.Repositories
+{
+    interface IGenericRepository<TEntity>
+        where TEntity : class
+    {
+        Task<IQueryable<TEntity>> ReadAllAsync();
+        Task<TEntity> ReadByIdAsync<TIdType>(TIdType Id);
+        Task CreatAsync(TEntity entity);
+        Task UpdateAsync<TIdType>(TIdType id, TEntity entity);
+
+    }
+}
