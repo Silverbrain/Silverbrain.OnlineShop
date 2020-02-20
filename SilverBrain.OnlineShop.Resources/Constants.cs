5e43295d86a26eb5d9d422d22f5701c59d7a66e2
silverbr <sinaataei@outlook.com>
2020-02-20 14:46:28 +0330

    fixes: -constants added -kendo numeric column filter fixed

---
+++ SilverBrain.OnlineShop.Resources/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Silverbrain.OnlineShop.Resources
+{
+    public static class Constants
+    {
+        public static string AreaManagement { get; set; } = "Dashboard";
+        public static string ControllerHome { get; set; } = "Home";
+        public static string ActionIndex { get; set; } = "Index";
+    }
+}
