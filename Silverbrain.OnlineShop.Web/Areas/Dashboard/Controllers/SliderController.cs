8741393d1afb75318386eb05ae6c8c332379f759
silverbr <sinaataei@outlook.com>
2020-04-06 18:19:52 +0430

    slider model added

---
+++ Silverbrain.OnlineShop.Web/Areas/Dashboard/Controllers/SliderController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Silverbrain.OnlineShop.ViewModels;
+
+namespace Silverbrain.OnlineShop.Web.Areas.Dashboard.Controllers
+{
+    public class SliderController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(SliderViewModel model)
+        {
+            return Json(true);
+        }
+    }
+}
